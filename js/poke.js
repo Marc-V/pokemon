e4b4c7baa1dfed4b8dedd39c5831532cd67c0b7e
Marc-V <mvsquez9@gmail.com>
2021-05-14 19:57:11 -0400

    Gotta Catch 'em All!

---
+++ js/poke.js
@@ -0,0 +1,11 @@
+var num ="";
+
+function drawPoke(){
+    for(var i=1; i<152;i++){
+        num += '<img src="http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/'+i+'.png">';
+
+    }
+    document.getElementById('pokedex').innerHTML = num;
+}
+
+drawPoke();
